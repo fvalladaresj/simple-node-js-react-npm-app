df63d0f31a5059604b490cd9341b481dfd21b781
fvalladaresj <fvalladares.j@gmail.com>
2022-07-27 01:31:45 -0400

    selenium tests

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -18,32 +18,3 @@ echo 'application renders satisfactorily. This command actually invokes the test
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
 # npm test
-
-# selenium tests
-const {Builder, By} = require('selenium-webdriver');
-const assert = require('assert');
-
-(async function firstScript() {
-  try {
-    let driver = await new Builder().forBrowser('chrome').build();
-
-    await driver.get('https://www.google.com');
-
-    await driver.getTitle();
-
-    await driver.manage().setTimeouts({implicit: 1000})
-
-    let searchBox = await driver.findElement(By.name('q'));
-    let searchButton = await driver.findElement(By.name('btnK'));
-
-    await searchBox.sendKeys('Selenium');
-    await searchButton.click();
-
-    let value = await searchBox.getAttribute("value");
-    assert.deepStrictEqual(value, "Selenium")
-
-    await driver.quit();
-  } catch (error) {
-    console.log(error)
-  }
-})();
